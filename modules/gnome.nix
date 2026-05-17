9ed35e965e032c38bf5d93dead09a0e49a181d80
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-17 12:24:04 -0500

    error:gnome.nix

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,9 +1,12 @@
 {
-  services.xserver.enable = true;   # Enable the X11 windowing system.
-  services.displayManager.gdm.enable = true;   # Enable the GNOME Desktop Environment.
-  services.desktopManager.gnome.enable = true;
-  services.xserver.xkb = {   # Configure keymap in X11
-    layout = "latam";
-    variant = "";
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    
+    xkb = {
+      layout = "latam";
+      variant = "";
+    };
   };
-}  
+}
