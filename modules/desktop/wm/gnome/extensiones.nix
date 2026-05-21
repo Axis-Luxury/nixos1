987d186097fc2368f07adb3660a2784b0e0f4070
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-21 18:40:28 -0500

    +gtop y pkgs, gnome-extensions en el mismo environment packages

--- modules/desktop/wm/gnome/extensiones.nix
+++ modules/desktop/wm/gnome/extensiones.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 
-{
-  environment.systemPackages = with pkgs.gnomeExtensions; [
-    appindicator
-    blur-my-shell
-    dash-to-dock
+{ 
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.system-monitor
+    gtop
   ];
 }
