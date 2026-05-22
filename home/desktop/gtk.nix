e78940b09d6ffe9664ea879907b2ef15fca1b32a
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-21 19:10:47 -0500

    gnome:gtk config envés de tweaks

---
+++ home/desktop/gtk.nix
@@ -0,0 +1,36 @@
+{pkgs,...}:
+{
+  gtk = {
+    enable = true;
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+
+    theme = {
+      name = "Tokyonight-Dark";
+      package = pkgs.tokyo-night-gtk;
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    cursorTheme = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+
+    font = {
+      name = "Inter";
+      size = 11;
+    };
+  };
+}
