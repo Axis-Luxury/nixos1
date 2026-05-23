c3c84bd5dd220f98e3751d7627b6d50908d5b486
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-22 20:24:48 -0500

    stylix first commit

---
+++ home/theme.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+let
+  assets = import ./assets.nix { inherit pkgs; };
+in
+{
+  imports = [
+    ./stylix.nix
+  ];
+
+  gtk = {
+    enable = true;
+
+    iconTheme = {
+      package = assets.kora;
+      name = "kora";
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+
+    package = assets.material-cursors;
+    name = "material_dark_cursors";
+    size = 24;
+  };
+
+  fonts.packages = [
+    assets.sf-pro
+    assets.jetbrains-mono
+  ];
+}
