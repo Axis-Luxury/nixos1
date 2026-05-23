73852019cba88474a3da96d4877d4370ee2e4666
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-22 20:13:58 -0500

    update dconf

--- home/desktop/dconf.nix
+++ home/desktop/dconf.nix
@@ -2,12 +2,16 @@
 
 {
   dconf.settings = {
-    "org/gnome/desktop/wm/preferences" = {
-      button-layout = ":minimize,maximize,close";
-    };
-
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+
+      cursor-theme = "material_dark_cursors";
+      icon-theme = "kora";
+
+      font-name = "SF Pro Display 11";
+      document-font-name = "SF Pro Text 11";
+
+      monospace-font-name = "JetBrainsMono Nerd Font 12";
     };
   };
 }
