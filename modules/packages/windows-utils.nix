feb9c0cfb5580dd8cd88c13835789e27451489ab
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-25 12:54:27 -0500

    -gnome.extensions.caefeine traslado

--- modules/packages/windows-utils.nix
+++ modules/packages/windows-utils.nix
@@ -1,18 +1,12 @@
-{pkgs, ...}:
+{config, pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
     gnome.baobab          # WinDirStat
     nemo                  # OneCommander
     eyedropper            # Color Picker
     easyeffects           # Nahimic
-    libwacom              # Wacom (config en Ajustes → Tableta de lápiz)
+    # libwacom              # Wacom (config en Ajustes → Tableta de lápiz)
     rclone                # Google Drive sync
-  ];
-
-  # Extensiones GNOME
-  services.gnome.gnome-browser-connector.enable = true;
-  environment.systemPackages = environment.systemPackages ++ [
-    pkgs.gnomeExtensions.caffeine   # Awake
   ];
 
   # Lenovo LOQ (LenovoLegionLinux)
