52fdd7f9d3e1755718e3d6847178bc2be28e1771
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-25 13:36:16 -0500

    modulos:windows-utils:easyeffects, lenovolegionlinux

--- modules/packages/windows-utils.nix
+++ modules/packages/windows-utils.nix
@@ -1,23 +1,11 @@
-{config, pkgs, ...}:
+{pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
     baobab          # WinDirStat
     nemo                  # OneCommander
     eyedropper            # Color Picker
-    easyeffects           # Nahimic
     # libwacom              # Wacom (config en Ajustes → Tableta de lápiz)
     rclone                # Google Drive sync
   ];
 
-  # Lenovo LOQ (LenovoLegionLinux)
-  boot.kernelModules = [ "legion_laptop" ];
-  services.hardware.openrgb.enable = true;  # si usas RGB
-
-  # PipeWire (necesario para EasyEffects)
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
 }
