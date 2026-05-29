09401fa1ae5d3210b212b601ab6a962fe7c6f3a1
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-29 12:43:15 -0500

    fix

--- modules/desktop/wm/gnome/exclude.nix
+++ modules/desktop/wm/gnome/exclude.nix
@@ -3,7 +3,7 @@
   services.gnome.games.enable = false; # Deshabilitar juegos de GNOME
   services.gnome.core-apps.enable = false; # Deshabilitar aplicaciones principales de GNOME
   services.gnome.core-developer-tools.enable = false; # Deshabilitar herramientas de desarrollo de GNOME
-  environment.gnome.excludePkgs = with pkgs; [
+  environment.gnome.excludePackages = with pkgs; [
     xterm
   ];
 }
