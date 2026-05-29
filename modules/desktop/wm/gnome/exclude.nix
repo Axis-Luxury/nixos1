5b6110bdafb8ca04ea79cd503e6f1c698e0ec7ee
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-29 13:27:38 -0500

    +excludes

--- modules/desktop/wm/gnome/exclude.nix
+++ modules/desktop/wm/gnome/exclude.nix
@@ -4,6 +4,17 @@
   services.gnome.core-apps.enable = true; # aplicaciones principales de GNOME
   services.gnome.core-developer-tools.enable = false; # herramientas de desarrollo de GNOME
   environment.gnome.excludePackages = with pkgs; [
+#    file-roller
+    geary
+    seahorse
+    snapshot
+    cheese
+    evince
+    epiphany
+    gnome-text-editor
+    gedit
+    gnome-user-docs
+    gnome-connections
     gnome-contacts
     gnome-maps
     gnome-music
