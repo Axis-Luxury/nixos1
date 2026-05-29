9bab070b72e9934ada53e29053bcfb11aabca21d
Axis-Luxury <alexisumaquinga548@gmail.com>
2026-05-29 11:08:03 -0500

    coment:displayManager de cosmic

--- modules/desktop/wm/cosmic/cosmic.nix
+++ modules/desktop/wm/cosmic/cosmic.nix
@@ -1,4 +1,4 @@
 {
-  services.displayManager.cosmic-greeter.enable = true;   # Enable the COSMIC login manager
+#  services.displayManager.cosmic-greeter.enable = true;   # Enable the COSMIC login manager
   services.desktopManager.cosmic.enable = true;   # Enable the COSMIC desktop environment
 }
